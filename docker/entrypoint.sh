6b96ea9c22b393698f9ef1e92e2597d07efc39c9
Ruben De Swaef <ruben@inuits.eu>
2021-08-25 16:02:07 +0200

    Init session oidc lib

---
+++ docker/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+echo "Starting oidc library"
+exec npm run dev
---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import scss from 'rollup-plugin-scss'
+import vue from 'rollup-plugin-vue'
+import packageJson from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      format: 'cjs',
+      file: packageJson.main,
+      sourcemap: true,
+      globals: { vue: 'Vue' },
+    },
+    {
+      format: 'esm',
+      file: packageJson.module,
+      sourcemap: true,
+      globals: { vue: 'Vue' },
+    },
+  ],
+  plugins: [vue(), typescript(), peerDepsExternal(), resolve(), commonjs(), scss()],
+  external: ['vue'],
+}
